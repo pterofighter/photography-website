7b3caec7c92b541eb550e19137e6670d5452b682
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-09-27 22:41:24 -0700

    figured out rendering images kind of

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/login', function(req, res, next) {
   res.render('login')
 })
 
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage')
+})
+
 module.exports = router;
