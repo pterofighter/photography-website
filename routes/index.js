206971b4e7c3e031bad07d788753391b8bfae61c
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-09-28 22:33:59 -0700

    got dispalying cards working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('homepage');
+  imageList = []
+  imageList.push({src: "images/photos/SAM_0394.JPG", name: "what"})
+  imageList.push({src: "images/photos/SAM_0394.JPG", name: "how"})
+  res.render('homepage', {imageList: imageList} );
 });
 
 router.get('/login', function(req, res, next) {
