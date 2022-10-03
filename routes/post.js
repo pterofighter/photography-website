61ae6b16f0286a044efe87995f935748d9056f6c
He (Damon) Chen <pterofighter1139283368@gmail.com>
2022-10-02 22:46:16 -0700

    made it so uploaded files will now have the proper extension

--- routes/post.js
+++ routes/post.js
@@ -6,13 +6,18 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "public/images/photos")
+        cb(null, "public/images/photos");
+    },
+    filename: function (req, file, cb) {
+        let fileExt = file.mimetype.split('/')[1];
+        cb(null, file.fieldname + "." + fileExt)
     }
 })
 
 var uploader = multer({storage: storage});
 
 router.post('/createPost', uploader.single("postImg"),(req, res, next) => {
+    console.log("AHHHHHHHHHHHHHHHH!")
     console.log(req);
     res.send('');
 }); 
